db64e9ec27c58a50360472bbd25ffa1c6a5e4796
Jen McQuade <jenmcquade@gmail.com>
2020-03-01 12:54:32 -0800

    Add messages to chatroom controller and render in index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ User.create(username: 'jonsnow', password: 'password')
 User.create(username: 'gandalf', password: 'password')
 User.create(username: 'jenny', password: 'password')
 User.create(username: 'frodo', password: 'password')
+
+Message.create(body: "This is a message from Jon Snow", user: User.find(2))
+Message.create(body: "Yo, is a message from Gandalf", user: User.find(3))
+Message.create(body: "Howdy, is a message from Jen", user: User.find(4))
+Message.create(body: "What's up, is a message from Frodo", user: User.find(5))
