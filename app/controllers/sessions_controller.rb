434b797163548a069e5323fd6c5345e22173a5cf
Jen McQuade <jenmcquade@gmail.com>
2020-03-03 10:36:21 -0800

    Create sign in functionality and update views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to root_path if logged_in?
+  end
 
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else 
+      flash.now[:danger] = 'There was something wrong ' \
+        'with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to root_path
   end
 end
