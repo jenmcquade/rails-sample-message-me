485b6100802311826fe8bf3199123c3db7b2a146
Jen McQuade <jenmcquade@gmail.com>
2020-03-05 12:39:03 -0800

    styling and controller actions for user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user, except: [:new]
+
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+  get 'signup', to: 'users#new'
+  get 'users', to: 'users#new'
+  post 'users', to: 'users#create'
 
   mount ActionCable.server, at: '/cable'
 end
